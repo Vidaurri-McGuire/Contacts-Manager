1e4aea739cd8539f2a172d511561c94bfc267e6c
Anthony V <avidaurri4@gmail.com>
2020-12-29 16:13:17 -0600

    contactsList getters/setters

---
+++ src/contactsManager/contactsList.java
@@ -0,0 +1,30 @@
+package contactsManager;
+
+public class contactsList {
+    // Declaring string variables
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public contactsList(String firstName, String lastName, String phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // First | Last | Phone #: output
+    public String toString(){return this.firstName + " | " + this.lastName + " | Phone #: " + this.phoneNumber;}
+
+    // Getters & Setters
+    public String getFirstName() {return firstName; }
+
+    public void setFirstName(String firstName){this.firstName = firstName;}
+
+    public String getLastName(){return lastName;}
+
+    public void setLastName(String lastName){this.lastName = lastName;}
+
+    public String getPhoneNumber(){return phoneNumber;}
+
+    public void setPhoneNumber(String phoneNumber){this.phoneNumber = phoneNumber;}
+}
